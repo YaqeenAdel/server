9f2e1c43baa9c2d4e491b34baf3cd0aec5b22b09
Haytham Abuelfutuh <haytham@afutuh.com>
2023-09-17 13:38:45 -0700

    Fix schema relationships

--- YaqeenDAL/Model/Model.cs
+++ YaqeenDAL/Model/Model.cs
@@ -4,165 +4,162 @@ namespace YaqeenDAL.Model
 {
     public class User : Entity
     {
+        [Key]
+        public int UserId { get; set; } // This attribute will contains required informations came from Auth0  
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string MobileNumber { get; set; }
+        
+        [Index(IsUnique = true)]
+        public string? MobileNumber { get; set; }
+
+        [Required]
+        [Index(IsUnique = true)]
         public string Email { get; set; }
         public bool AgreedTerms { get; set; }
-        public bool? Gender { get; set; }
-        public string IdpUserIdentifier { get; set; }  // This attribute will contains required informations came from Auth0  
+        public string? Gender { get; set; }
         public bool IsEmailVerified { get; set; }
         public bool IsDeleted { get; set; }
         public bool IsActive { get; set; }
 
-
-        public ICollection<VerificationCode> VerificationCodes { get; set; }
-
         // Navigation Properties
-        public Patient Patient { get; set; }
-        public Doctor Doctor { get; set; }
+        [ForeignKey("UserId")]
+        public virtual Patient Patient { get; set; }
+        [ForeignKey("UserId")]
+        public virtual Doctor Doctor { get; set; }
+        public virtual ICollection<AreaofInterest> AreaofInterests { get; set; }
     }
 
     public class Patient : Entity
     {
+        [Key]
         public int UserId { get; set; }
         public int AgeGroup { get; set; }
+
+        public int CancerTypeId { get; set; }
+
+        [ForeignKey("CancerStage")]
+        public int CancerStageId { get; set; }
 
         public ICollection<PatientAreaofInterest> AreaofInterests { get; set; }
         public ICollection<Question> Questions { get; set; }
         public ICollection<Bookmark> Bookmarks { get; set; }
 
         // Navigation Property
-        public User User { get; set; }
-        public CancerType CancerType { get; set; }
-        public CancerStage? CancerStage { get; set; }
-
+        [ForeignKey("UserId")]
+        public virtual User User { get; set; }
+        [ForeignKey("CancerId")]
+        public virtual CancerType? CancerType { get; set; }
+        [ForeignKey("StageId")]
+        public virtual CancerStage? CancerStage { get; set; }
     }
 
     public class Doctor : Entity
     {
+        [Key]
         public int UserId { get; set; }
         public string University { get; set; }
         public string Degree { get; set; }
         public string MedicalField { get; set; }
-        public bool IsVerified { get; set; }  //verfication as adoctor from his certifications 
-        public ICollection<DoctorAreaofInterest> AreaofInterests { get; set; }
-        public ICollection<Answer> Answers { get; set; }
-        public ICollection<Bookmark> Bookmarks { get; set; }
-
+        public VerificationStatus VerificationStatus { get; set; }
+        
         // Navigation Property
-        public User User { get; set; }
+        [ForeignKey("AnswerId")]
+        public virtual ICollection<Answer> Answers { get; set; }
+        [ForeignKey("BookmarkId")]
+        public virtual ICollection<Bookmark> Bookmarks { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual User User { get; set; }
     }
 
     public class CancerType : AuditableEntity
     {
-        [ForeignKey("Patient")]
-
+        [Key]
         public int CancerId { get; set; }
-
         public string CancerTypeName { get; set; }
-
-        // Navigation Property
-        public Patient Patient { get; set; }
-
     }
+
     public class CancerStage : AuditableEntity
     {
-
+        [Key]
         public int StageId { get; set; }
-
         public string StageName { get; set; }
-
-        // Navigation Property
-        public Patient? Patient { get; set; }
     }
 
     public class AreaofInterest : AuditableEntity
     {
+        [Key]
         public int AreaId { get; set; }
         public string AreaName { get; set; }
         public string Logo { get; set; }
 
-        // Navigation Properties
-        public Patient Patient { get; set; }
-        public Doctor Doctor { get; set; }
-
-    }
-    public class PatientAreaofInterest
-    {
-        public int UserId { get; set; }
-        public Patient Patient { get; set; }
-
-        public int AreaId { get; set; }
-
-        // Navigation Property
-
-        public AreaofInterest AreaofInterest { get; set; }
-    }
-
-    public class DoctorAreaofInterest
-    {
-        public int UserId { get; set; }
-        public Doctor Doctor { get; set; }
-
-        public int AreaId { get; set; }
-        public AreaofInterest AreaofInterest { get; set; }
+        [ForeignKey("UserId")]
+        public virtual ICollection<User> Users { get; set; }
     }
 
     public class Question : AuditableEntity
     {
-        public int PatientId { get; set; }
+        [Key]
+        public int QuestionId { get; set; }
+        public int UserId { get; set; }
         public string Title { get; set; }
         public string Category { get; set; }
         public string Description { get; set; }
-        public ICollection<Answer> Answers { get; set; }
-
+        
         // Navigation Property
-        public Patient Patient { get; set; }
+        public virtual ICollection<Answer> Answers { get; set; }
+        [ForeignKey("UserId")]
+        public virtual User User { get; set; }
     }
 
     public class Answer : AuditableEntity
     {
+        [Key]
         public int AnswerId { get; set; }
         public int DoctorId { get; set; }
         public int QuestionId { get; set; }
         public string Content { get; set; }
 
         // Navigation Properties
-        public Doctor Doctor { get; set; }
-        public Question Question { get; set; }
+        [ForeignKey("DoctorId")]
+        public virtual Doctor Doctor { get; set; }
+        public virtual Question Question { get; set; }
     }
 
     public class Article : AuditableEntity
     {
+        [Key]
         public int ArticleId { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
         public string Category { get; set; }
-        public ICollection<Bookmark> Bookmarks { get; set; }
+        public virtual ICollection<Bookmark> Bookmarks { get; set; }
     }
 
     public class Bookmark : AuditableEntity
     {
+        [Key]
         public int BookmarkId { get; set; }
         public int UserId { get; set; }
         public int? ArticleId { get; set; }
         public string Type { get; set; } // Can be "Question" or "Article"
 
         // Navigation Properties
-        public Patient Patient { get; set; }
-        public Doctor Doctor { get; set; }
-        public Article Article { get; set; }
+        [ForeignKey("UserId")]
+        public virtual Patient Patient { get; set; }
+        [ForeignKey("UserId")]
+        public virtual Doctor Doctor { get; set; }
+        public virtual Article Article { get; set; }
     }
 
-    public class VerificationCode : Entity
+    public class VerificationStatus 
     {
-        public int UserId { get; set; }
-        public string Code { get; set; }
-        public DateTime ExpirationDate { get; set; }
+        public int VerifierUserId { get; set; }
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+        public string Notes { get; set; }
 
-        // Navigation Property
-        public User User { get; set; }
+        [ForeignKey("VerifierUserId")]
+        public virtual User Verifier { get; set; }
     }
-
 }
--- YaqeenDAL/Model/YaqeenDbContext.cs
+++ YaqeenDAL/Model/YaqeenDbContext.cs
@@ -21,88 +21,87 @@ namespace YaqeenDAL.Model
         public DbSet<Answer> Answers { get; set; }
         public DbSet<Article> Articles { get; set; }
         public DbSet<Bookmark> Bookmarks { get; set; }
-        public DbSet<VerificationCode> VerificationCodes { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             // Configure the primary keys
-            modelBuilder.Entity<User>().HasKey(u => u.Id);
-            modelBuilder.Entity<Patient>().HasKey(p => p.Id);
-            modelBuilder.Entity<Doctor>().HasKey(d => d.Id);
-            modelBuilder.Entity<CancerStage>().HasKey(d => d.StageId);
-            modelBuilder.Entity<CancerType>().HasKey(d => d.CancerId);
-            modelBuilder.Entity<AreaofInterest>().HasKey(d => d.AreaId);
+            // modelBuilder.Entity<User>().HasKey(u => u.Id);
+            // modelBuilder.Entity<Patient>().HasKey(p => p.Id);
+            // modelBuilder.Entity<Doctor>().HasKey(d => d.Id);
+            // modelBuilder.Entity<CancerStage>().HasKey(d => d.StageId);
+            // modelBuilder.Entity<CancerType>().HasKey(d => d.CancerId);
+            // modelBuilder.Entity<AreaofInterest>().HasKey(d => d.AreaId);
 
-            modelBuilder.Entity<Question>().HasKey(q => q.Id);
-            modelBuilder.Entity<Answer>().HasKey(a => a.AnswerId);
-            modelBuilder.Entity<Article>().HasKey(a => a.ArticleId);
-            modelBuilder.Entity<Bookmark>().HasKey(b => b.BookmarkId);
-            modelBuilder.Entity<VerificationCode>().HasKey(v => v.Id);
-
+            // modelBuilder.Entity<Question>().HasKey(q => q.Id);
+            // modelBuilder.Entity<Answer>().HasKey(a => a.AnswerId);
+            // modelBuilder.Entity<Article>().HasKey(a => a.ArticleId);
+            // modelBuilder.Entity<Bookmark>().HasKey(b => b.BookmarkId);
 
             // Define relationships
-            modelBuilder.Entity<User>()
-                .HasOne(u => u.Patient)
-                .WithOne(p => p.User)
-                .HasForeignKey<Patient>(p => p.UserId);
+            // modelBuilder.Entity<User>()
+            //     .HasOne(u => u.Patient)
+            //     .WithOne(p => p.User)
+            //     .HasForeignKey<Patient>(p => p.UserId);
 
-            modelBuilder.Entity<User>()
-                .HasOne(u => u.Doctor)
-                .WithOne(d => d.User)
-                .HasForeignKey<Doctor>(d => d.UserId);
+            // modelBuilder.Entity<User>()
+            //     .HasOne(u => u.Doctor)
+            //     .WithOne(d => d.User)
+            //     .HasForeignKey<Doctor>(d => d.UserId);
 
-            modelBuilder.Entity<Patient>()
-                .HasMany(p => p.Questions)
-                .WithOne(q => q.Patient)
-                .HasForeignKey(q => q.PatientId);
+            // modelBuilder.Entity<Patient>()
+            //     .HasMany(p => p.Questions)
+            //     .WithOne(q => q.Patient)
+            //     .HasForeignKey(q => q.PatientId);
 
-            modelBuilder.Entity<Patient>()
-                .HasMany(p => p.Bookmarks)
-                .WithOne(b => b.Patient)
-                .HasForeignKey(b => b.UserId);
+            // modelBuilder.Entity<Patient>()
+            //     .HasMany(p => p.Bookmarks)
+            //     .WithOne(b => b.Patient)
+            //     .HasForeignKey(b => b.UserId);
+
+            // modelBuilder.Entity<Doctor>()
+            //     .HasMany(d => d.Answers)
+            //     .WithOne(a => a.Doctor)
+            //     .HasForeignKey(a => a.DoctorId);
+
+            // modelBuilder.Entity<Doctor>()
+            //     .HasMany(d => d.Bookmarks)
+            //     .WithOne(b => b.Doctor)
+            //     .HasForeignKey(b => b.UserId);
+
+            // modelBuilder.Entity<Question>()
+            //     .HasMany(q => q.Answers)
+            //     .WithOne(a => a.Question)
+            //     .HasForeignKey(a => a.QuestionId);
+
+            // modelBuilder.Entity<Article>()
+            //     .HasMany(a => a.Bookmarks)
+            //     .WithOne(b => b.Article)
+            //     .HasForeignKey(b => b.ArticleId);
+
+            // modelBuilder.Entity<User>()
+            //     .HasMany(u => u.VerificationCodes)
+            //     .WithOne(v => v.User)
+            //     .HasForeignKey(v => v.UserId);
+
+            // modelBuilder.Entity<Patient>()
+            // .HasOne(cs => cs.CancerStage)
+            // .WithOne(p => p.Patient)
+            // .HasForeignKey<CancerStage>(p => p.StageId)
+            //  .IsRequired(false);
+
+            // modelBuilder.Entity<Patient>()
+            // .HasOne(ct => ct.CancerType)
+            // .WithOne(p => p.Patient)
+            // .HasForeignKey<CancerType>(p => p.CancerId);
+
+            // modelBuilder.Entity<PatientAreaofInterest>()
+            //     .HasKey(pa => new { pa.UserId, pa.AreaId });
+
+            // modelBuilder.Entity<DoctorAreaofInterest>()
+            //     .HasKey(da => new { da.UserId, da.AreaId });
 
             modelBuilder.Entity<Doctor>()
-                .HasMany(d => d.Answers)
-                .WithOne(a => a.Doctor)
-                .HasForeignKey(a => a.DoctorId);
-
-            modelBuilder.Entity<Doctor>()
-                .HasMany(d => d.Bookmarks)
-                .WithOne(b => b.Doctor)
-                .HasForeignKey(b => b.UserId);
-
-            modelBuilder.Entity<Question>()
-                .HasMany(q => q.Answers)
-                .WithOne(a => a.Question)
-                .HasForeignKey(a => a.QuestionId);
-
-            modelBuilder.Entity<Article>()
-                .HasMany(a => a.Bookmarks)
-                .WithOne(b => b.Article)
-                .HasForeignKey(b => b.ArticleId);
-
-            modelBuilder.Entity<User>()
-                .HasMany(u => u.VerificationCodes)
-                .WithOne(v => v.User)
-                .HasForeignKey(v => v.UserId);
-
-            modelBuilder.Entity<Patient>()
-            .HasOne(cs => cs.CancerStage)
-            .WithOne(p => p.Patient)
-            .HasForeignKey<CancerStage>(p => p.StageId)
-             .IsRequired(false);
-
-            modelBuilder.Entity<Patient>()
-            .HasOne(ct => ct.CancerType)
-            .WithOne(p => p.Patient)
-            .HasForeignKey<CancerType>(p => p.CancerId);
-
-            modelBuilder.Entity<PatientAreaofInterest>()
-                .HasKey(pa => new { pa.UserId, pa.AreaId });
-
-            modelBuilder.Entity<DoctorAreaofInterest>()
-                .HasKey(da => new { da.UserId, da.AreaId });
-
+               .OwnsOne(r => r.VerificationStatus);
         }
     }
 }
