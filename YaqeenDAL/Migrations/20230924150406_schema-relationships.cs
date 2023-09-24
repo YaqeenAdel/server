4c851cdeec612050073fd8d8be30aa8703fa9b2d
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-24 15:04:07 +0000

    Migration Changes

---
+++ YaqeenDAL/Migrations/20230924150406_schema-relationships.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YaqeenApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class schemarelationships : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AnswerId",
+                table: "Doctors",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BookmarkId",
+                table: "Doctors",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AnswerId",
+                table: "Doctors");
+
+            migrationBuilder.DropColumn(
+                name: "BookmarkId",
+                table: "Doctors");
+        }
+    }
+}
