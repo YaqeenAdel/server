531da53ac344e82187ac07a1f34908a6e1e4498a
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-25 01:20:16 +0000

    Migration Changes

--- YaqeenDAL/Migrations/20230925010611_schema-relationships.Designer.cs
+++ YaqeenDAL/Migrations/20230925010611_schema-relationships.Designer.cs
@@ -12,7 +12,7 @@ using YaqeenDAL.Model;
 namespace YaqeenApi.Migrations
 {
     [DbContext(typeof(YaqeenDbContext))]
-    [Migration("20230925010611_schema-relationships")]
+    [Migration("20230925012014_schema-relationships")]
     partial class schemarelationships
     {
         /// <inheritdoc />
