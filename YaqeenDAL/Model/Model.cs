8dbdf6d237ad1f692cd86448c3bbd0a0a9b64f53
Haytham Abuelfutuh <haytham@afutuh.com>
2023-09-30 21:35:04 -0700

    correctly map doctor to questions/answers
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>

--- YaqeenDAL/Model/Model.cs
+++ YaqeenDAL/Model/Model.cs
@@ -63,13 +63,9 @@ namespace YaqeenDAL.Model
         public string Degree { get; set; }
         public string MedicalField { get; set; }
         public VerificationStatus VerificationStatus { get; set; }
-        public int AnswerId { get; set; }
-        public int BookmarkId { get; set; }
         
         // Navigation Property
-        [ForeignKey("AnswerId")]
         public virtual ICollection<Answer> Answers { get; set; }
-        [ForeignKey("BookmarkId")]
         public virtual ICollection<Bookmark> Bookmarks { get; set; }
 
         [ForeignKey("UserId")]
