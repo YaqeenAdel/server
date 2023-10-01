f468ace7435875d987af803d39ce10145b7572d2
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2023-10-01 17:45:50 +0000

    Migration Changes

---
+++ YaqeenDAL/Migrations/20231001174541_optional-verification-status.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YaqeenDAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class optionalverificationstatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
