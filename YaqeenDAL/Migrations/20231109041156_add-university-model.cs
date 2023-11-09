955821c6fa0034bd9ddef002cd86fa5f31303469
Haytham Abuelfutuh <haytham@afutuh.com>
2023-11-09 04:12:03 +0000

    Migration Changes

---
+++ YaqeenDAL/Migrations/20231109041156_add-university-model.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace YaqeenDAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class adduniversitymodel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Universities",
+                columns: table => new
+                {
+                    UniversityId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CountryName = table.Column<string>(type: "text", nullable: false),
+                    CountryCode = table.Column<string>(type: "text", nullable: false),
+                    StateCode = table.Column<string>(type: "text", nullable: false),
+                    StateName = table.Column<string>(type: "text", nullable: false),
+                    UniversityName = table.Column<string>(type: "text", nullable: false),
+                    Id = table.Column<int>(type: "integer", nullable: false),
+                    Active = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    LastModifiedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Universities", x => x.UniversityId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Universities_CountryCode",
+                table: "Universities",
+                column: "CountryCode");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Universities_CountryCode_StateCode",
+                table: "Universities",
+                columns: new[] { "CountryCode", "StateCode" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Universities");
+        }
+    }
+}
