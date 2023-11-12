10a2c8902e90f462691c49ae2f8008250edc2c76
Haytham Abuelfutuh <haytham@afutuh.com>
2023-11-12 19:48:05 +0000

    Migration Changes

---
+++ YaqeenDAL/Migrations/20231112194758_questions.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace YaqeenDAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class questions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contents",
+                columns: table => new
+                {
+                    ContentId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ParentContentId = table.Column<int>(type: "integer", nullable: true),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<byte[]>(type: "bytea", rowVersion: true, nullable: false),
+                    UpdatedAt = table.Column<byte[]>(type: "bytea", rowVersion: true, nullable: true),
+                    DeletedAt = table.Column<byte[]>(type: "bytea", rowVersion: true, nullable: true),
+                    Raw = table.Column<Dictionary<string, string>>(type: "jsonb", nullable: false),
+                    AuthorUserId = table.Column<string>(type: "text", nullable: false),
+                    AssignedTo = table.Column<int>(type: "integer", nullable: true),
+                    Phase = table.Column<int>(type: "integer", nullable: false),
+                    Tags = table.Column<string[]>(type: "text[]", nullable: false),
+                    Id = table.Column<int>(type: "integer", nullable: false),
+                    Active = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contents", x => x.ContentId);
+                    table.ForeignKey(
+                        name: "FK_Contents_Contents_ParentContentId",
+                        column: x => x.ParentContentId,
+                        principalTable: "Contents",
+                        principalColumn: "ContentId");
+                    table.ForeignKey(
+                        name: "FK_Contents_Users_AuthorUserId",
+                        column: x => x.AuthorUserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contents_AssignedTo",
+                table: "Contents",
+                column: "AssignedTo");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contents_AuthorUserId",
+                table: "Contents",
+                column: "AuthorUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contents_ParentContentId",
+                table: "Contents",
+                column: "ParentContentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contents_Tags",
+                table: "Contents",
+                column: "Tags");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contents_Type",
+                table: "Contents",
+                column: "Type");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contents");
+        }
+    }
+}
