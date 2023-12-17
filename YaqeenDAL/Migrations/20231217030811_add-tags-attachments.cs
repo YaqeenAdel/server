37160d85e132f3cd8b6daba30bc38dfdaef3b21d
Haytham Abuelfutuh <haytham@afutuh.com>
2023-12-16 22:09:07 -0500

    fix migrations (#38)
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>

---
+++ YaqeenDAL/Migrations/20231217030811_add-tags-attachments.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using YaqeenDAL.Model;
+
+#nullable disable
+
+namespace YaqeenDAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class addtagsattachments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Attachment>(
+                name: "Attachments",
+                table: "Contents",
+                type: "jsonb",
+                nullable: false);
+
+            migrationBuilder.AddColumn<Dictionary<string, string>>(
+                name: "Tags",
+                table: "Contents",
+                type: "jsonb",
+                nullable: false);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contents_Tags",
+                table: "Contents",
+                column: "Tags");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Contents_Tags",
+                table: "Contents");
+
+            migrationBuilder.DropColumn(
+                name: "Attachments",
+                table: "Contents");
+
+            migrationBuilder.DropColumn(
+                name: "Tags",
+                table: "Contents");
+        }
+    }
+}
