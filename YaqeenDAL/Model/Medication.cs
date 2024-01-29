10a8c96350d5dac3538a80b964dee8cbce14775e
Haytham Abuelfutuh <haytham@afutuh.com>
2024-01-29 06:58:50 -0800

    Add translation to symptom lookup
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>

--- YaqeenDAL/Model/Medication.cs
+++ YaqeenDAL/Model/Medication.cs
@@ -131,6 +131,7 @@ namespace YaqeenDAL.Model
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int SymptomLookupId { get; set; }
         public string Name { get; set; }
+        public string TranslationId { get; set; }
     }
 
     public class Symptom : Entity
