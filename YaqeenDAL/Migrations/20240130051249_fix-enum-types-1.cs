bbb31a5ac847ca222c27cf7ec3216be9fe3360ee
Haytham Abuelfutuh <haytham@afutuh.com>
2024-01-29 21:13:51 -0800

    Actually use enum types
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>

---
+++ YaqeenDAL/Migrations/20240130051249_fix-enum-types-1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YaqeenDAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixenumtypes1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Schedules_EntityType_UserId",
+                table: "Schedules");
+
+            migrationBuilder.DropColumn(
+                name: "EntityType",
+                table: "Schedules");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EntityType",
+                table: "Schedules",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedules_EntityType_UserId",
+                table: "Schedules",
+                columns: new[] { "EntityType", "UserId" });
+        }
+    }
+}
