d3681963972645a59da747bb9f8f986aa4df7fcd
Haytham Abuelfutuh <haytham@afutuh.com>
2024-02-10 07:59:11 -0800

    Fix cron check migration
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>

--- YaqeenDAL/Model/YaqeenDbContext.cs
+++ YaqeenDAL/Model/YaqeenDbContext.cs
@@ -194,7 +194,7 @@ namespace YaqeenDAL.Model
             modelBuilder.Entity<Schedule>(entity => entity
                 .ToTable(t => t
                     .HasCheckConstraint("CronExpression",
-                        @"CronExpression ~* '/(@(annually|yearly|monthly|weekly|daily|hourly|reboot))|(@every (\d+(ns|us|µs|ms|s|m|h))+)|((((\d+,)+\d+|(\d+(\/|-)\d+)|\d+|\*) ?){5})/'")));
+                        @"""CronExpression"" ~* '/(@(annually|yearly|monthly|weekly|daily|hourly|reboot))|(@every (\d+(ns|us|µs|ms|s|m|h))+)|((((\d+,)+\d+|(\d+(\/|-)\d+)|\d+|\*) ?){5})/'")));
         }
     }
 }
