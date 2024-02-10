231de9326138a75ecbf4ef314545dd4fbd11f1a1
Haytham Abuelfutuh <haytham@afutuh.com>
2024-02-10 08:03:33 -0800

    Actually commit the cron expression migration fix (#73)
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>

---
+++ YaqeenDAL/Migrations/20240210160213_cron-another-iteration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YaqeenDAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class cronanotheriteration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CronExpression",
+                table: "Schedules");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CronExpression",
+                table: "Schedules",
+                sql: "\"CronExpression\" ~* '/(@(annually|yearly|monthly|weekly|daily|hourly|reboot))|(@every (\\d+(ns|us|µs|ms|s|m|h))+)|((((\\d+,)+\\d+|(\\d+(\\/|-)\\d+)|\\d+|\\*) ?){5})/'");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CronExpression",
+                table: "Schedules");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CronExpression",
+                table: "Schedules",
+                sql: "CronExpression ~* '/(@(annually|yearly|monthly|weekly|daily|hourly|reboot))|(@every (\\d+(ns|us|µs|ms|s|m|h))+)|((((\\d+,)+\\d+|(\\d+(\\/|-)\\d+)|\\d+|\\*) ?){5})/'");
+        }
+    }
+}
