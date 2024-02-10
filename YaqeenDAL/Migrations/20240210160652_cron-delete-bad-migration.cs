7ec5d32f3cdb7b2908c37b65ef2368e09bb1e800
Haytham Abuelfutuh <haytham@afutuh.com>
2024-02-10 08:07:15 -0800

    Delete bad migration
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>

--- YaqeenDAL/Migrations/20240210160652_cron-delete-bad-migration.cs
+++ YaqeenDAL/Migrations/20240210160652_cron-delete-bad-migration.cs
@@ -5,7 +5,7 @@
 namespace YaqeenDAL.Migrations
 {
     /// <inheritdoc />
-    public partial class cronvalidator : Migration
+    public partial class crondeletebadmigration : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
@@ -13,7 +13,7 @@ namespace YaqeenDAL.Migrations
             migrationBuilder.AddCheckConstraint(
                 name: "CronExpression",
                 table: "Schedules",
-                sql: "CronExpression ~* '/(@(annually|yearly|monthly|weekly|daily|hourly|reboot))|(@every (\\d+(ns|us|µs|ms|s|m|h))+)|((((\\d+,)+\\d+|(\\d+(\\/|-)\\d+)|\\d+|\\*) ?){5})/'");
+                sql: "\"CronExpression\" ~* '/(@(annually|yearly|monthly|weekly|daily|hourly|reboot))|(@every (\\d+(ns|us|µs|ms|s|m|h))+)|((((\\d+,)+\\d+|(\\d+(\\/|-)\\d+)|\\d+|\\*) ?){5})/'");
         }
 
         /// <inheritdoc />
